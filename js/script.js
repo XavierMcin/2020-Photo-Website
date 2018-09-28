eae89864aa29e418e3fa3715531c951a13c14de4
xavier <xavier.mcin@gmail.com>
2018-09-28 17:25:34 -0400

    pull out menu base styles

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 var hamburgerMenu = document.querySelector('.hamburger-menu');
+var slideMenu = document.querySelector('aside');
 
 hamburgerMenu.addEventListener('click', function() {
 
 	hamburgerMenu.classList.toggle('clicked');
+	slideMenu.classList.toggle('slider');
 
 
 })
