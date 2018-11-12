edc079ef0397f4058f83e3de70ed30825d0521cb
xavier <xavier.mcin@gmail.com>
2018-11-12 15:16:32 -0500

    added some javascript, trying to figure out how to make each image a trigger for the modal function

--- js/script.js
+++ js/script.js
@@ -10,6 +10,9 @@ hamburgerMenu.addEventListener('click', function() {
 })
 
 
+
+
+
 // var pageIntro = document.querySelector('.loader');
 
 
@@ -22,3 +25,55 @@ hamburgerMenu.addEventListener('click', function() {
 
 
 // })
+
+
+
+
+
+/***** MODAL WINDOW EVENTS *****/
+
+
+var modal = document.querySelector('.modal');
+var modalTrigger = document.querySelector('.column-one img');
+var modalImage = document.querySelector('.modal-image')
+var pageDoc = document.querySelector('body');
+var firstImg = document.querySelector('.modal-image img:first-of-type');
+var secondImg = document.querySelector('.modal-image img:nth-of-type(2)');
+
+
+
+
+
+
+modalTrigger.addEventListener('click', function() {
+
+	modal.classList.add('open');
+	modalImage.classList.add('open');
+	pageDoc.classList.add('noscroll');
+
+	if (modalTrigger.matchesSelector == ('.column img:first-of-type')) {
+		firstImg.classList.add('show');
+	} else if (modalTrigger.matchesSelector == ('.column img:nth-of-type(2)')) {
+		secondImg.classList.add('show');
+	}
+
+	
+
+})
+
+
+modal.addEventListener('click', function() {
+
+	modal.classList.remove('open');
+	modalImage.classList.remove('open');
+	pageDoc.classList.remove('noscroll');
+
+	if (modalTrigger.matchesSelector == ('.column img:first-of-type')) {
+		firstImg.classList.remove('show');
+	} else if (modalTrigger.matchesSelector == ('.column img:nth-of-type(2)')) {
+		secondImg.classList.remove('show');
+	}
+
+
+
+})
